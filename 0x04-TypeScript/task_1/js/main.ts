d63305c48134e5582421718150194fa0a60931cf
Kenneth Onaiwu <kenzee4u@gmail.com>
2025-09-14 07:51:54 +0100

    Added function for printTeacher in main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,14 +24,13 @@ const director1: Director = {
 console.log(director1);
 
 // Define interface for the function
-interface printTeacher {
+interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// Implement the function
-const printTeacher: printTeacher = (firstName, lastName) => {
+function printTeacher(firstName: string, lastName: string): string {
   return '${firstName.charAt(0)}. ${lastName}';
-};
+}
 
 // Example usage
 console.log(printTeacher("John", "Doe"));
